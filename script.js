7d3f8944b00885e2f613212a630b837c5b1eb824
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-06 21:33:11 -0800

    added last 3 elements

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ console.log(myArray[myArray.length - 1]);
 
 for (let i = 0; i < myArray.length; i++) { console.log(myArray[i]);
 }
+/*The new values appear at the end of the array because push() adds elements to the end. */
+
+myArray.push(60, 70, 80, 90, 100); console.log(myArray);
+
+/* The last three elements (100, 90, 80) were removed from the array because pop() removes the last element. */
+
+myArray.pop(); myArray.pop(); myArray.pop(); console.log(myArray);
+
+let myString = "Welcome2thedungeon"; console.log(myString.length); // Output: 17
