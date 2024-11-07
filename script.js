eb4a56f701751ac498c06d6b5168d2644de91618
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-06 21:32:32 -0800

    Input total number of elements

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 let myArray = [10, 20, 30, 40, 50];
 
 console.log(myArray[0]);
+
+/* For the last value, I used myArray.length - 1 because myArray.length gives the total number of elements, and subtracting 1 gets the index of the last element. */
+
+console.log(myArray[myArray.length - 1]);
+
+for (let i = 0; i < myArray.length; i++) { console.log(myArray[i]);
+}
